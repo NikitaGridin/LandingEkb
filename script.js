67257621cb1c3e43dad35d30297f72f4682bc747
Nikita <aruisialalin@yandex.ru>
2023-07-14 15:35:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,190 @@
+const advantagesinfo = [
+  {
+    img: "./img/quality.jpg",
+    title: "Качество деталей",
+    desc: "Получаем запчасти с грузовиков из Европы без пробега по России. В Европе условия эксплуатации лучше, поэтому износ деталей меньше. Не разбираем автомобили старше 2011 года выпуска, поэтому у наших запчастей большой остаточный ресурс.",
+  },
+  {
+    img: "./img/assortiment.jpg",
+    title: "Широкий ассортимент",
+    desc: "Привозим грузовики из Европы на разбор каждый месяц. Разбираем Scania 5 и 6 серии. В наличии всё от болта до кабины. Если чего-то нет в Екатеринбурге, доставим из другого филиала или закажем под вас из Европы.",
+  },
+  {
+    img: "./img/garanty.jpg",
+    title: "Гарантия и доставка",
+    desc: "Даём гарантию 30 дней на двигатель и КПП, на остальные детали и агрегаты – 14 дней. Детали храним с соблюдением необходимых условий. Отправим посылку в любую точку страны и ближнего зарубежья удобной транспортной компанией в день оплаты.",
+  },
+  {
+    img: "./img/exp.png",
+    title: "Опыт",
+    desc: "Разбираем и восстанавливаем автомобили Scania более 7 лет. Начинали в Нижнем Новгороде, потом открыли филиалы в Екатеринбурге и Москве. Концентрируемся на грузовиках одной марки, поэтому знаем о них всё: от «болячек» до особенностей ремонта и установки деталей.  ",
+  },
+];
+
+const categoriesInfo = [
+  {
+    img: "./img/kabina_category.webp",
+    title: "Кабины и комплектующие",
+    url: "https://worktruck.ru/catalog/katalog-zapchastei-scania/kabina",
+  },
+  {
+    img: "./img/dvigatel_category.webp",
+    title: "Двигатели",
+    url: "https://worktruck.ru/catalog/katalog-zapchastei-scania/dvigatel",
+  },
+  {
+    img: "./img/kpp_category.webp",
+    title: "Кпп",
+    url: "https://worktruck.ru/catalog/katalog-zapchastei-scania/kontrolnye-pribory",
+  },
+  {
+    img: "./img/reduktor_category.webp",
+    title: "Редукторы",
+    url: "https://worktruck.ru/catalog/katalog-zapchastei-scania/kontrolnye-pribory/zadnii-most/redkutor-v-sbore",
+  },
+  {
+    img: "./img/oblicovka_category.webp",
+    title: "Облицовка",
+    url: "https://worktruck.ru/catalog/katalog-zapchastei-scania/kabina/reshyotka-radiatora-kapot/oblitsovka-radiatora",
+  },
+  {
+    img: "./img/svetoteh_category.webp",
+    title: "Светотехника",
+    url: "https://worktruck.ru/catalog/katalog-zapchastei-scania/elektrika/osveschenie",
+  },
+];
+
+const productsInfo = [
+  {
+    img: "./img/product.webp",
+    title:
+      "Фара лев. в сборе с рамкой, уплотнением и крышкой (6 серия) подходит для грузовиков марки Scania/Скания",
+    url: "https://worktruck.ru/products/fara-lev-v-sbore-s-ramkoy-uplotneniem-i-kryshkoy-6-seriya",
+    article: "Арт: 2655848",
+    price: "135 000,00 ₽",
+  },
+  {
+    img: "./img/kabina.webp",
+    title:
+      "Кабина CS20 H (синяя 1260 mm) (6 серия) подходит для грузовиков марки Scania/Скания",
+    url: "https://worktruck.ru/products/kabina-cs20-h-sinyaya-1260-mm-6-seriya",
+    article: "Арт: 2151876",
+    price: "135 000,00 ₽",
+  },
+  {
+    img: "./img/fonar.webp",
+    title:
+      "Задний комбинированный фонарь прав. (светодиодный) с сигнализатором заднего хода",
+    url: "https://worktruck.ru/products/zadniy-kombinirovannyy-fonar-prav-svetodiodnyy-s-signalizatorom-zadnego-hoda",
+    article: "Арт: 2380953",
+    price: "15 000,00 ₽",
+  },
+  {
+    img: "./img/capot.webp",
+    title:
+      "Капот в сборе CS (6 серия) подходит для грузовиков марки Scania/Скания",
+    url: "https://worktruck.ru/products/kapot-v-sbore-cs-6-seriya",
+    article: "Арт: 2894752",
+    price: "430 000,00 ₽",
+  },
+  {
+    img: "./img/spoiler.webp",
+    title: "Верхний спойлер с расширениями 53 см (6 серия)",
+    url: "https://worktruck.ru/products/verhniy-spoyler-s-rasshireniem-53-sm-6-seriya",
+    article: "Арт: 2390445",
+    price: "110 000,00 ₽",
+  },
+  {
+    img: "./img/koleso.webp",
+    title: "Колесо 315/80/22,5 в сборе с диском (ведущая ось)",
+    url: "https://worktruck.ru/products/koleso-315-80-22-5-v-sbore-s-diskom-veduschaya-os",
+    article: "Арт: 2655848",
+    price: "315/80/22,5",
+  },
+  {
+    img: "./img/reductor.webp",
+    title:
+      "Комплект редукторов gr 3.96 1/4.38.1 RBP835, RB835 подходит для грузовиков марки Scania/Скания",
+    url: "https://worktruck.ru/products/komplekt-reduktorov-gr3-96-1-4-38-1-rbp835-rb835",
+    article: "Арт: 1531355",
+    price: "740 000,00 ₽",
+  },
+  {
+    img: "./img/dvigatel.webp",
+    title:
+      "Двигатель DC13 121 440 hp PDE EEV подходит для грузовиков марки Scania/Скания",
+    url: "https://worktruck.ru/products/dvigatel-scania-dc13-121-440-hp-pde",
+    article: "Арт: 2200477",
+    price: "1 500 000,00 ₽",
+  },
+];
+
+const advantages = document.querySelector("#advantages");
+const categories = document.querySelector("#categories");
+const products = document.querySelector("#products");
+const burger_menu = document.querySelector("#burger_menu");
+const burger_btn = document.querySelector("#burger_btn");
+
+burger_btn.addEventListener("click", () => {
+  burger_menu.classList.add("block");
+  burger_menu.classList.remove("hidden");
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    (!event.target.closest("#burger_menu") &&
+      !event.target.closest("#burger_btn")) ||
+    event.target.closest("#burger_btn__close") ||
+    event.target.closest("#burger_menu a")
+  ) {
+    burger_menu.classList.add("hidden");
+  }
+});
+
+const advantageElements = advantagesinfo.map((advantage) => {
+  return `
+    <div class="shadow-lg rounded-[20px]">
+    <img src="${advantage.img}" alt="" class="w-full h-[300px] rounded-xl"/>
+    <div class="px-[20px] py-[24px]">
+    <div class="text-[24px] font-semibold leading-[100%] mb-[10px] md:text-[32px] lg:text-[24px]">${advantage.title}</div>
+    <div class="text-[20px] font-light text-justify md:text-[24px] lg:text-[20px]">${advantage.desc}</div>
+    </div>
+    </div>
+    `;
+});
+
+const categorieElements = categoriesInfo.map((category) => {
+  return `
+<a href="${category.url}" class="relative bg-no-repeat bg-cover rounded-[30px] py-5 px-[30px] self-start shadow-xl transition-all hover:-translate-y-1 min-h-[300px] hover:shadow-2xl flex flex-col justify-end h-full" style="background-image: url('${category.img}')">
+    <h2 class="text-[32px] leading-[100%] font-bold mb-[10px] xl:text-[24px] z-10">
+      ${category.title}
+    </h2>
+
+  <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-60 rounded-[30px] z-0"></div>
+</a>
+    `;
+});
+
+const productsElements = productsInfo.map((product) => {
+  return `
+    <a
+    href="${product.url}"
+    class="rounded-[20px] bg-cover pb-[20px] self-start relative shadow-xl transition-all hover:-translate-y-1 hover:shadow-2xl"
+    >
+    <img src="${product.img}" alt="" class="rounded-[20px] w-full h-[300px]" />
+    <div class="px-[20px] mt-[28px]">
+    <h2
+    class="text-[20px] leading-[100%] font-medium mb-[10px] truncate"
+    >
+    ${product.title}
+    </h2>
+    <div class="text-[16px] font-light mb-[10px] text-gray-400">${product.article}</div>
+    <div class="text-[24px] font-medium">${product.price}</div>
+    </div>
+    </a>
+    `;
+});
+
+advantages.innerHTML = advantageElements.join("");
+products.innerHTML = productsElements.join("");
+categories.innerHTML = categorieElements.join("");
